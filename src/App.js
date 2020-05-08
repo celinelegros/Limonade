b80c22630cf3a26fbd6141630583a87404b0505e
celinelegros <celinelegros1993@gmail.com>
2020-05-08 15:34:43 +0200

    style(added h1):

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>When life gives you lemon ...</h1>
+      <h2>Create good communication !</h2>
+    </>
   );
 }
 
